36c06906c71db1350c175afd18a9ecbff097b4ab
jordyf15 <jordyferdian88@gmail.com>
2021-08-19 13:29:44 +0700

    create hover change color effect on the sketch grids

--- script/script.js
+++ script/script.js
@@ -1,7 +1,18 @@
 const sketchContainer = document.querySelector('#sketch-container');
 
+
+let color = '#000000';
+
+
+
+function changeGridColorOnHover() {
+    console.log('hovered');
+    this.style.backgroundColor = color;
+}
+
 for(let i =0;i<16;i++){
     const sketchGrid = document.createElement('div');
     sketchGrid.classList.add('sketch-grid');
+    sketchGrid.addEventListener('mouseover', changeGridColorOnHover);
     sketchContainer.appendChild(sketchGrid);
 }
